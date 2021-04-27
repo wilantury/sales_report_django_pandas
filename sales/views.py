7673a1070ebdd92cae365418dc1a03cee4f28e97
wilantury <ventas@antury.net>
2021-04-27 18:44:28 -0500

    [ADD] new class based view into Sales (ListView)

--- sales/views.py
+++ sales/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Sale
 
 def home_view(request):
     hello = 'Hello world from view'
-    return render(request, 'sales/main.html', {'hello': hello})
+    return render(request, 'sales/home.html', {'hello': hello})
+
+class SaleListView(ListView):
+    model = Sale
+    template_name = 'sales/main.html'
+    #context_object_name = 'another_name_object_as_param'
