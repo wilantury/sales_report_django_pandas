83b86691f4ea29a1ea682adb031911f291b63bc2
wilantury <ventas@antury.net>
2021-04-27 17:59:55 -0500

    [ADD] reports model

--- reports/models.py
+++ reports/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from profiles.models import Profile
+class Report(models.Model):
+    name = models.CharField(max_length=120)
+    image = models.ImageField(upload_to='reports', blank=True)
+    remarks = models.TextField()
+    author = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
 
-# Create your models here.
+    def __str__(self):
+        return str(self.name)
