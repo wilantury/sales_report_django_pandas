f9bc4ef1051778eb376e27ea9ce9dbf0421add34
wilantury <ventas@antury.net>
2021-04-27 18:25:38 -0500

    [ADD] starter sales view, and test the template pattern

--- sales/views.py
+++ sales/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home_view(request):
+    hello = 'Hello world from view'
+    return render(request, 'sales/main.html', {'hello': hello})
