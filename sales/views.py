44775a6a11c948f7067210af439544ed328205ff
wilantury <ventas@antury.net>
2021-04-28 16:49:02 -0500

    [ADD] receive data from form

--- sales/views.py
+++ sales/views.py
@@ -5,9 +5,15 @@ from .forms import SalesSearchForm
 
 def home_view(request):
     form = SalesSearchForm(request.POST or None)
-    hello = 'Hello world from view'
+
+    if request.method == 'POST':
+        date_from = request.POST.get('date_from')
+        date_to = request.POST.get('date_to')
+        chart_type = request.POST.get('chart_type')
+        print(date_from, date_to, chart_type)
+
+
     context = {
-        'hello': hello,
         'form': form
     }
     return render(request, 'sales/home.html', context)
