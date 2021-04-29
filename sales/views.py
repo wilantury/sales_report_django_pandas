bef7104c713f86b2136c771ca7ae2c80b4441e30
wilantury <ventas@antury.net>
2021-04-28 21:18:15 -0500

    [ADD] make query .filter .all .get, create a DataFrame using Pandas

--- sales/views.py
+++ sales/views.py
@@ -1,7 +1,12 @@
+# Django
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
+# Models
 from .models import Sale
+# Forms
 from .forms import SalesSearchForm
+# Pandas
+import pandas as pd
 
 def home_view(request):
     form = SalesSearchForm(request.POST or None)
@@ -10,7 +15,22 @@ def home_view(request):
         date_from = request.POST.get('date_from')
         date_to = request.POST.get('date_to')
         chart_type = request.POST.get('chart_type')
-        print(date_from, date_to, chart_type)
+    
+        # qs = Sale.objects.all()
+        qs = Sale.objects.filter(created__date=date_from)
+        #obj = Sale.objects.get(id=1)
+        # print(qs)
+        # print(obj)
+        # print(qs.values())
+        # print(qs.values_list())
+        print("#"*20)
+        df1 = pd.DataFrame(qs.values())
+        print(df1)
+        print("#"*20)
+        # df2 = pd.DataFrame(qs.values_list())
+        # print(df2)
+        #print("#"*20)
+
 
 
     context = {
@@ -27,7 +47,7 @@ class SaleDetailView(DetailView):
     model = Sale
     template_name = 'sales/detail.html'
 """
-This a function view approach. 
+This is a function view approach. 
 def sale_list_view(request): # use it instead of ListView
     qs = Sale.objects.all()
     return render(request, 'sales/main.html',{"object_list": qs})
