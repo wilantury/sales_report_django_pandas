d3b20549c535cba6a0204999825d268bfa12e444
wilantury <ventas@antury.net>
2021-05-03 19:19:41 -0500

    [ADD] get the products list of all sales opertaions get_positions, not seprate yet, list all positios into a html page

--- sales/views.py
+++ sales/views.py
@@ -10,6 +10,7 @@ import pandas as pd
 
 def home_view(request):
     sales_df = None
+    positions_df = None
     form = SalesSearchForm(request.POST or None)
 
     if request.method == 'POST':
@@ -18,15 +19,29 @@ def home_view(request):
         chart_type = request.POST.get('chart_type')
     
         # qs = Sale.objects.all()
-        qs = Sale.objects.filter(created__date__lte=date_to, created__date__gte=date_from)
-        if len(qs) > 0:
+        sale_qs = Sale.objects.filter(created__date__lte=date_to, created__date__gte=date_from)
+
+        if len(sale_qs) > 0:
             #obj = Sale.objects.get(id=1)
             # print(obj)
             # print(qs.values())
             # print(qs.values_list())
-            sales_df = pd.DataFrame(qs.values())
-            print(sales_df)
+            positions_data = []
+            for sale in sale_qs:
+                for pos in sale.get_positions():
+                    obj = {
+                        'position_id': pos.id,
+                        'product': pos.product.name,
+                        'quantity': pos.quantity,
+                        'price': pos.price
+                    }
+                    positions_data.append(obj)
+            sales_df = pd.DataFrame(sale_qs.values())
             sales_df = sales_df.to_html()
+            positions_df = pd.DataFrame(positions_data)
+            positions_df = positions_df.to_html()
+
+
         else:
             print('no data')
 
@@ -34,7 +49,8 @@ def home_view(request):
 
     context = {
         'form': form,
-        'sales_df': sales_df
+        'sales_df': sales_df,
+        'positions_df':positions_df
     }
     return render(request, 'sales/home.html', context)
 
