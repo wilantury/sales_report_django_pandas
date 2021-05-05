54de7ea62a39256c6ae69d3a8ad7f531262a5887
wilantury <ventas@antury.net>
2021-05-04 20:46:41 -0500

    [ADD] get a DataFrame grouped by

--- sales/views.py
+++ sales/views.py
@@ -14,6 +14,7 @@ def home_view(request):
     sales_df = None
     positions_df = None
     merged_df = None
+    df = None
     form = SalesSearchForm(request.POST or None)
 
     if request.method == 'POST':
@@ -51,11 +52,13 @@ def home_view(request):
             positions_df = pd.DataFrame(positions_data)
 
             merged_df = pd.merge(sales_df, positions_df, on='sales_id')
-            print(merged_df)
+            df = merged_df.groupby('transaction_id', as_index=False)['price'].agg(sum)
+            print(df)
             sales_df = sales_df.to_html()
             positions_df = positions_df.to_html()
             merged_df = merged_df.to_html()
-
+            df = df.to_html()
+        
 
         else:
             print('no data')
@@ -66,7 +69,8 @@ def home_view(request):
         'form': form,
         'sales_df': sales_df,
         'positions_df':positions_df,
-        'merged_df':merged_df
+        'merged_df':merged_df,
+        'df':df
     }
     return render(request, 'sales/home.html', context)
 
