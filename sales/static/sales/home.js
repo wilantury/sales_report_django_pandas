944204feb2d7f8dcca1d24c4757665a976fb9b70
wilantury <ventas@antury.net>
2021-05-07 10:08:40 -0500

    [ADD] put image into modal using JavaScript

--- sales/static/sales/home.js
+++ sales/static/sales/home.js
@@ -1,5 +1,6 @@
 const reportBtn = document.getElementById('report-btn') 
 const img = document.getElementById('img')
+const modalBody = document.getElementById('modal-body')
 
 console.log(reportBtn)
 console.log(img)
@@ -7,3 +8,9 @@ console.log(img)
 if (img){
     reportBtn.classList.remove('not-visible')
 }
+
+reportBtn.addEventListener('click', ()=>{
+    console.log('Clicked')
+    img.setAttribute('class', 'w-100')
+    modalBody.prepend(img)
+})
