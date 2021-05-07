a1ae52c58c5febf5000fbec699a9025aa4e7d7c8
wilantury <ventas@antury.net>
2021-05-07 14:07:06 -0500

    [ADD] create view to save a report into database. get the image, get ready the image to save as .png file

--- reports/views.py
+++ reports/views.py
@@ -1,3 +1,37 @@
+# Django
 from django.shortcuts import render
+from django.http import JsonResponse
+# Models
+from profiles.models import Profile
+from .models import Report
+# Forms
+from .forms import ReportForm
+# utils
+from .utils import get_report_image
 
-# Create your views here.
+def create_report_view(request):
+    if request.is_ajax():
+        # name = request.POST.get('name')
+        # remarks = request.POST.get('remarks')
+        # image = request.POST.get('image')
+
+        # img = get_report_image(image)
+
+        # author = Profile.objects.get(user=request.user)
+
+        # Report.objects.create(name=name, remarks=remarks, image=img, author=author)
+        
+        """another way to save the data into database, using Forms Models"""
+        form =ReportForm(request.POST or None)
+        image = request.POST.get('image')
+        img = get_report_image(image)        
+        author = Profile.objects.get(user=request.user)
+
+        if form.is_valid():
+            instance = form.save(commit=False)
+            instance.image = img
+            instance.author = author
+            instance.save()
+
+        return JsonResponse({'data':'send'})
+    return JsonResponse({'res':'No ajax request'})
--- sales/static/sales/home.js
+++ sales/static/sales/home.js
@@ -1,16 +1,46 @@
 const reportBtn = document.getElementById('report-btn') 
 const img = document.getElementById('img')
 const modalBody = document.getElementById('modal-body')
+const reportName = document.getElementById('id_name')
+const reportRemarks = document.getElementById('id_remarks')
+const reportForm = document.getElementById('report-form')
 
-console.log(reportBtn)
-console.log(img)
+const csrf = document.getElementsByName('csrfmiddlewaretoken')[0].value
+
+console.log(csrf)
+console.log(reportName)
+console.log(reportRemarks)
 
 if (img){
     reportBtn.classList.remove('not-visible')
 }
 
 reportBtn.addEventListener('click', ()=>{
-    console.log('Clicked')
     img.setAttribute('class', 'w-100')
     modalBody.prepend(img)
+
+    reportForm.addEventListener('submit', e=>{
+        e.preventDefault()
+        const formData = new FormData()
+        formData.append('csrfmiddlewaretoken', csrf)
+        formData.append('name',reportName.value)
+        formData.append('remarks', reportRemarks.value)
+        formData.append('image', img.src)
+
+        $.ajax({
+            type: 'POST',
+            url:'/reports/save/',
+            data: formData,
+            success: response => {
+                console.log(response)
+            },
+            error: error => {
+                console.log(error)
+            },
+            processData: false,
+            contentType: false,
+        })
+    })
 })
+
+
